8a5d11364d12cd080cd1c226dd9fca316c2075dc
Katja Weber <katja.elli.weber@googlemail.com>
2026-06-17 13:44:29 +0200

    added ingest pipeline and refactored tests.

--- tests/test_judge.py
+++ tests/test_judge.py
@@ -10,23 +10,34 @@ client = OpenAI()
 ytt_api = YouTubeTranscriptApi()
 
 from gapfinder_agent.yt_agent import create_agent, run_agent, GapFinderAgentConfig
+from gapfinder_agent.ingest import YouTubePipeline, VideoMetadataService, TranscriptService, StorageService, ChunkService
 from gapfinder_agent.tools import GapFinderAgentTools
 
-from tests.utils import run_agent_test
 from tests.judge import assert_criteria
 
+storage_service = StorageService()
+
+pipeline = YouTubePipeline(
+    storage_service=storage_service,
+    chunk_service=ChunkService(storage_service),
+    chunk_documents_fn=chunk_documents,
+)
+
+index = pipeline.create_rag_index()
 
 @pytest.fixture(scope="module")
 def agent(output_type=None):
     tools = GapFinderAgentTools(
         client=client,
-        model="gpt-4o-mini",
-        ytt_api=ytt_api,
-        chunk_func=chunk_documents,
-        index_cls=Index
+        model='gpt-4o',
+        index_cls=index
     )
+    
+    agent_config = GapFinderAgentConfig(
+        client=client,
+        model='gpt-4o'
+        )
    
-    agent_config = GapFinderAgentConfig()
     agent = create_agent(agent_config, tools, output_type=output_type)
 
     return agent
@@ -35,11 +46,10 @@ def agent(output_type=None):
 @pytest.mark.asyncio
 async def test_judge_agent_uses_tools(agent):
     user_prompt = 'What is this video about: https://www.youtube.com/watch?v=wjZofJX0v4M?'
-    result = await run_agent_test(agent, user_prompt)
+    result = await run_agent(agent, user_prompt)
 
     await assert_criteria(result, [
         "makes at least 2 tool calls",
         "provides key concepts of the video in the output",
         "asks the user if they want to explore the concepts",
-        "suggests questions for the concepts"
     ])
